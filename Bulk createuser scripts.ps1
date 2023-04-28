62366d50eb5c8216d6a6544810df5aabfb953c71
Vincent Fernandes <vincyferns2088@gmail.com>
2023-04-28 16:00:36 +0530

    scriptadded

---
+++ Bulk createuser scripts.ps1
@@ -0,0 +1,9 @@
+﻿$users= import-csv "C:\Users\vincy\Downloads\users.csv"
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "Monday@123"
+
+foreach($user in $users)
+{
+write-host "The display name for this user is $($user.displayname)" -ForegroundColor Green
+New-AzureADUser -DisplayName $user.DisplayName -PasswordProfile $PasswordProfile -UserPrincipalName $user.UserPrincipalName -AccountEnabled $true -MailNickName $user.MailNickName
+}
